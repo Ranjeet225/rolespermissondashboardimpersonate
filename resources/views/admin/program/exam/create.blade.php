dee6a6de153310190ecae8f260d49a391acd8353
ranjeet <95184006+Ranjeet225@users.noreply.github.com>
2024-06-03 14:52:46 +0530

    das

--- resources/views/admin/program/exam/create.blade.php
+++ resources/views/admin/program/exam/create.blade.php
@@ -38,27 +38,12 @@
                                     </div>
                                     <div class="col-12">
                                         <label>Number<span class="text-danger">*</span></label>
-                                        <input type="number" class="form-control " name="number">
+                                        <input type="number" class="form-control " name="number" >
                                         @error('number')
                                             <div class="text-danger">{{ $message }}</div>
                                         @enderror
                                     </div>
-                                     <div class="col-12">
-+                                        <label>Program Level<span class="text-danger">*</span></label>
-+                                        <input type="text" class="form-control " name="program_level">
-+                                        @error('program_level')
-+                                            <div class="text-danger">{{ $message }}</div>
-+                                        @enderror
-+                                    </div>
-+                                    <div class="col-12">
-+                                        <label>Number<span class="text-danger">*</span></label>
-+                                        <input type="number" class="form-control " name="number">
-+                                        @error('number')
-+                                            <div class="text-danger">{{ $message }}</div>
-+                                        @enderror
-+                                    </div>
-+                                    <div class="col-12">
-<<<<<<<  db1e6d2d-59e4-4b70-b0d8-44e05b589832  >>>>>>>
+                                    <div class="col-12">
                                         <label>Enter Exam Name<span class="text-danger">*</span></label>
                                         <input type="text" class="form-control " name="name">
                                         @error('name')
